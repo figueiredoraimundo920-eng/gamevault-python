205896c091d9da75fb72bdc1fa8a1803193c0082
figueiredoraimundo920-eng <figueiredoraimundo920@gmail.com>
2026-08-20 16:04:20 -0300

    v0.3 - Adicionada validação das opções

--- gamevault.py
+++ gamevault.py
@@ -6,7 +6,11 @@ print("1 - Adicionar jogo")
 print("2 - Listar jogos")
 print("3 - Sair")
 
-escolha = int(input("Escolha o que quer: "))
+escolha = int(input("escolha o que quer "))
+
+while escolha > 3 or escolha < 1:
+    print("error 402")
+    escolha = int(input("(opção não identificada escolha novamente ou saia.)"))
 
 if escolha == 1:
     print("Adicionando jogo...")
