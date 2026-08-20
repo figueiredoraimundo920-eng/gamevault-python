e54ae682d3db203c3a48fe0439abe7e090bb0b90
figueiredoraimundo920-eng <figueiredoraimundo920@gmail.com>
2026-08-20 15:56:07 -0300

    v0.2 - Adicionada interação ao menu

--- gamevault.py
+++ gamevault.py
@@ -5,3 +5,12 @@ print("========================")
 print("1 - Adicionar jogo")
 print("2 - Listar jogos")
 print("3 - Sair")
+
+escolha = int(input("Escolha o que quer: "))
+
+if escolha == 1:
+    print("Adicionando jogo...")
+elif escolha == 2:
+    print("Listando jogos...")
+else:
+    print("Saindo...")
