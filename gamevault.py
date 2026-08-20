173397188c05a1925c9e23920d9a493f15252cfd
figueiredoraimundo920-eng <figueiredoraimundo920@gmail.com>
2026-08-20 16:04:38 -0300

    v0.5 - Adicionado cadastro básico de jogos

--- gamevault.py
+++ gamevault.py
@@ -6,24 +6,42 @@ print("1 - Adicionar jogo")
 print("2 - Listar jogos")
 print("3 - Sair")
 
-escolha = int(input("escolha o que quer "))
+escolha = int(input(""))
 while escolha > 3 or escolha < 1:
     print("error 402")
     escolha = int(input("(opção não identificada escolha novamente ou saia.)"))
 
 if escolha == 1:
     print("Adicionando jogo...")
+    while escolha != 3:
+        if escolha == 1:
+            nome = input("Qual o nome do jogo? ")
+            plataforma = input("plataforma? ")
+            nota = input("nota: ")
+            print(nome, "//nota:", nota, "//plataforma jogavel:", plataforma)
+
+            if escolha == 3:
+                print("saindo...")
+            print("========================")
+            print("       GAMEVAULT")
+            print("========================")
+
+            print("1 - Adicionar jogo")
+            print("2 - Listar jogos")
+            print("3 - Sair")
+            escolha = int(input(""))
 elif escolha == 2:
     print("Listando jogos...")
+    while escolha != 3:
+        if escolha == 3:
+            print("saindo...")
+        print("========================")
+        print("       GAMEVAULT")
+        print("========================")
+
+        print("1 - Adicionar jogo")
+        print("2 - Listar jogos")
+        print("3 - Sair")
+        escolha = int(input(""))
 else:
     print("Saindo...")
-
-while escolha != 3:
-    print("========================")
-    print("       GAMEVAULT")
-    print("========================")
-
-    print("1 - Adicionar jogo")
-    print("2 - Listar jogos")
-    print("3 - Sair")
-    escolha = int(input(""))
